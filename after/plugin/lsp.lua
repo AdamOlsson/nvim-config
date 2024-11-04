969bfd4e650c5b8f3843cbed245d57ad7e2fad74
Adam Olsson <adam.olsson.3@volvocars.com>
2024-11-04 09:47:30 +0100

    Setup lsp manually and add conform for formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,8 +1,41 @@
-local lsp = require('lspconfig')
+local lspconfig = require('lspconfig')
 
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+
+        local client_id = args.data.client_id
+        local client = vim.lsp.get_client_by_id(client_id)
+
+        -- if client.supports_method('textDocument/completion') then
+        --     -- Enable auto-completion
+        --     vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+        -- end
+
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            -- group = get_augroup(client),
+            buffer = args.buf,
+            callback = function()
+                require("conform").format({
+                    bufnr = args.buf,
+                    id = client.id
+                })
+            end
+        })
     end,
 })
 
+-- https://github.com/stevearc/conform.nvim?tab=readme-ov-file#setup
+
+lspconfig.lua_ls.setup {}
+
+lspconfig.rust_analyzer.setup {
+    settings = {
+        imports = {
+            group = {
+                enable = false
+            }
+        }
+    }
+}
--- lua/adamolsson/packer.lua
+++ lua/adamolsson/packer.lua
@@ -10,21 +10,24 @@ return require('packer').startup(function(use)
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use { 'rose-pine/neovim', as = 'rose-pine' }
-    use { 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
+    use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
     use { 'nvim-treesitter/playground' }
     use { 'mbbill/undotree' }
     use { 'tpope/vim-fugitive' }
 
-    use 'williamboman/mason.nvim'    
-    use 'williamboman/mason-lspconfig.nvim'
-    use 'neovim/nvim-lspconfig' 
-    use 'simrat39/rust-tools.nvim'
+    use 'neovim/nvim-lspconfig'
+    use({
+        "stevearc/conform.nvim",
+        config = function()
+            require("conform").setup()
+        end,
+    })
 
-    use 'hrsh7th/nvim-cmp' 
+    use 'hrsh7th/nvim-cmp'
 
     -- LSP completion source:
     use 'hrsh7th/cmp-nvim-lsp'
@@ -32,11 +35,10 @@ return require('packer').startup(function(use)
     -- Useful completion sources:
     use 'hrsh7th/cmp-nvim-lua'
     use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-vsnip'                             
-    use 'hrsh7th/cmp-path'                              
-    use 'hrsh7th/cmp-buffer'                            
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/vim-vsnip'
 
-    use {"akinsho/toggleterm.nvim", tag = '*' }
-
+    use { "akinsho/toggleterm.nvim", tag = '*' }
 end)
